8d2f388c554e764c4b3bc2bb87a161a4fded9fed
Devansh-G <75668039+Devansh-G@users.noreply.github.com>
2023-04-17 04:51:56 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,7 +10,51 @@ using namespace std;
 
 class Solution{
     public:
-    void dfs(vector<string> &v, string &s, int i, int j, vector<vector<int>> & arr, int &n)
+    // void dfs(vector<string> &v, string &s, int i, int j, vector<vector<int>> & arr, int &n)
+    // {
+    //     if(i==n-1 && j==n-1)
+    //     {
+    //         v.push_back(s);
+    //         return;
+    //     }
+        
+    //     // int ax[] = { 0, 0, 1, -1};
+    //     // int ay[] = { 1, -1, 0, 0};
+    //     // char letter[] ={ 'R' , 'L' , 'D' , 'U' };
+        
+    //     int ax[] = { 1, 0, 0, -1};
+    //     int ay[] = { 0, -1, 1, 0};
+    //     char letter[] ={ 'D' , 'L' , 'R' , 'U' };
+    //     for(int k=0;k<4;k++)
+    //     {
+    //         int nx = i+ ax[k] , ny = j + ay[k];
+    //         if(nx>=0 && nx<n && ny>=0 && ny <n && arr[nx][ny]==1)
+    //         {
+    //             arr[nx][ny]=0;
+    //             s+=letter[k];
+    //             dfs(v,s,nx,ny,arr,n);
+    //             arr[nx][ny]=1;
+    //             s.pop_back();
+    //         }
+    //     }
+    //     return;
+    // }
+    // vector<string> findPath(vector<vector<int>> &m, int n) {
+    //     // Your code goes here
+    //     string s="";
+    //     vector<string> v;
+    //     if(m[0][0]==0)
+    //         return v;
+    //     m[0][0]=0;
+    //     dfs(v,s,0,0,m,n);
+    //     //sort(v.begin(),v.end());        // We wont need to sort this array if we took the lexiographical order from the start
+    //     // INSTEAD OF LINE 21-23 WE WRITE LINE 25 - 27
+    //     return v;
+    // }
+    
+    //Without DFS, and instead using visited array
+    
+    void fun(vector<string> &v, vector<vector<int>> &vis, string &s, int i, int j, vector<vector<int>> & arr, int &n)
     {
         if(i==n-1 && j==n-1)
         {
@@ -28,12 +72,12 @@ class Solution{
         for(int k=0;k<4;k++)
         {
             int nx = i+ ax[k] , ny = j + ay[k];
-            if(nx>=0 && nx<n && ny>=0 && ny <n && arr[nx][ny]==1)
+            if(nx>=0 && nx<n && ny>=0 && ny <n && arr[nx][ny]==1 && vis[nx][ny]==0)
             {
-                arr[nx][ny]=0;
+                vis[nx][ny]=1;
                 s+=letter[k];
-                dfs(v,s,nx,ny,arr,n);
-                arr[nx][ny]=1;
+                fun(v,vis,s,nx,ny,arr,n);
+                vis[nx][ny]=0;
                 s.pop_back();
             }
         }
@@ -43,14 +87,14 @@ class Solution{
         // Your code goes here
         string s="";
         vector<string> v;
+        vector<vector<int>> vis(n,vector<int>(n,0));
         if(m[0][0]==0)
             return v;
         m[0][0]=0;
-        dfs(v,s,0,0,m,n);
-        //sort(v.begin(),v.end());        // We wont need to sort this array if we took the lexiographical order from the start
-        // INSTEAD OF LINE 21-23 WE WRITE LINE 25 - 27
+        fun(v,vis,s,0,0,m,n);
         return v;
     }
+    
 };
 
     
