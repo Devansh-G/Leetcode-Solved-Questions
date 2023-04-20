c82ce4d9a324cb12caeff35e39a926c6df4ca97e
Devansh-G <75668039+Devansh-G@users.noreply.github.com>
2023-04-20 20:00:37 +0530

    Added solution - LeetHub

--- Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.cpp
@@ -8,22 +8,37 @@ class Solution
     public:
     //Function to count number of ways to reach the nth stair.
     //using dp
-    long long int fun(int n, vector<long long int> &dp)
-    {
-        if(n==0 || n==1)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        int left = fun(n-1, dp);
-        int right = fun(n-2, dp);
-        long int mod=1e9;
-        return dp[n] = (left + right) % (mod + 7 );
-    }
+    // long long int fun(int n, vector<long long int> &dp)
+    // {
+    //     if(n==0 || n==1)
+    //         return 1;
+    //     if(dp[n]!=-1)
+    //         return dp[n];
+    //     int left = fun(n-1, dp);
+    //     int right = fun(n-2, dp);
+    //     long int mod=1e9;
+    //     return dp[n] = (left + right) % (mod + 7 );
+    // }
     int countWays(int n)
     {
-        // your code here
-        vector <long long int> dp(n + 1,-1);
-        return fun(n,dp);
+        // using memoization
+        // vector <long long int> dp(n + 1,-1);
+        // return fun(n,dp);
+        
+        //Space optimisation
+        if(n==0 || n==1)
+            return 1;
+        long prev =1;
+        long prev2=1;
+        long ans=0, mod = 1e9;
+        for(int i=2;i<=n;i++)
+        {
+            ans=prev + prev2;
+            ans%=(mod +7);
+            prev2=prev;
+            prev=ans;
+        }
+        return ans;
     }
 };
 
