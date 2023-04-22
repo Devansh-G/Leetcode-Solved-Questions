1e5284eed93fc88d6c269c3c32aa23aec99b9181
Devansh-G <75668039+Devansh-G@users.noreply.github.com>
2023-04-23 02:42:58 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -6,25 +6,40 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int fun(vector<int> &height, vector<int> &dp, int n, int &k)
-    {
-        if(n==0)
-            return 0;
-        else if(dp[n]!=-1)
-            return dp[n];
-        int ans=INT_MAX;
-        for(int i=1;i<=k;i++)
-        {
-            if(n-i<0)
-                break;
-            ans= min(ans, fun(height, dp, n-i, k) + abs(height[n] - height[n-i]));
-        }
-        return dp[n]=ans;
-    }
+    // int fun(vector<int> &height, vector<int> &dp, int n, int &k)
+    // {
+    //     if(n==0)
+    //         return 0;
+    //     else if(dp[n]!=-1)
+    //         return dp[n];
+    //     int ans=INT_MAX;
+    //     for(int i=1;i<=k;i++)
+    //     {
+    //         if(n-i<0)
+    //             break;
+    //         ans= min(ans, fun(height, dp, n-i, k) + abs(height[n] - height[n-i]));
+    //     }
+    //     return dp[n]=ans;
+    // }
     int minimizeCost(vector<int>& height, int n, int k) {
         // Code here
-        vector<int> dp(n+1,-1);
-        return fun(height, dp, n-1, k);
+        //Using DP - Memoiization
+        // vector<int> dp(n+1,-1);
+        // return fun(height, dp, n-1, k);
+        
+        //Tabulation
+        vector<int> dp(n, INT_MAX);
+        dp[0]=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=k;j++)
+            {
+                if(i+j>=n)
+                    break;
+                dp[i+j]= min(dp[i+j],dp[i] + abs(height[i] - height[i+j]));
+            }
+        }
+        return dp[n-1];
     }
 };
 
