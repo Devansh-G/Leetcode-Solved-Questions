4864048a3d2e1780d7ea6e4e0132549764e385b2
Devansh-G <75668039+Devansh-G@users.noreply.github.com>
2023-06-28 02:45:44 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -10,31 +10,63 @@ using namespace std;
 class Solution{
 public:
     //           MEMIOZATION
-    int fun(vector<vector<int>> &grid, vector<vector<int>> &dp, int i, int j, int &n)
-    {
-        if(j<0 || j>=n)
-            return -1e9;
-        if(i==0)
-            return dp[i][j] =  grid[i][j];
+    // int fun(vector<vector<int>> &grid, vector<vector<int>> &dp, int i, int j, int &n)
+    // {
+    //     if(j<0 || j>=n)
+    //         return -1e9;
+    //     if(i==0)
+    //         return dp[i][j] =  grid[i][j];
         
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
         
-        int s= fun(grid, dp, i-1,j,n);
-        int ld= fun(grid, dp, i-1,j-1,n);
-        int rd= fun(grid, dp, i-1,j+1,n);
+    //     int s= fun(grid, dp, i-1,j,n);
+    //     int ld= fun(grid, dp, i-1,j-1,n);
+    //     int rd= fun(grid, dp, i-1,j+1,n);
         
-        return dp[i][j] =  grid[i][j] + max(s, max(ld,rd));
-    }
+    //     return dp[i][j] =  grid[i][j] + max(s, max(ld,rd));
+    // }
     int maximumPath(int N, vector<vector<int>> Matrix)
     {
         // code here
         //      MEMOIZATION
+        // vector<vector<int>> dp(N, vector<int> (N,-1));
+        // int maxi=fun(Matrix, dp, N-1, 0,N);
+        // for(int j=1 ; j<N ; j++)
+        //     maxi = max(maxi, fun(Matrix, dp, N-1, j,N));
+        // return maxi;
+        
+        
+        //      TABULATION
         vector<vector<int>> dp(N, vector<int> (N,-1));
-        int maxi=fun(Matrix, dp, N-1, 0,N);
-        for(int j=1 ; j<N ; j++)
-            maxi = max(maxi, fun(Matrix, dp, N-1, j,N));
+        for(int i=0;i<N;i++)
+        {
+            for(int j=0;j<N;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j] = Matrix[i][j];
+                    continue;
+                }
+                
+                int rd =-1e9, ld=-1e9;
+                int s = Matrix[i][j] + dp[i-1][j];
+                if(j>0) 
+                    ld= Matrix[i][j] + dp[i-1][j-1];
+                
+                if(j<N-1) 
+                    rd = Matrix[i][j] + dp[i-1][j+1];
+                    
+                dp[i][j] = max(s, max(rd,ld));
+            }
+        }
+        
+        int maxi=dp[N-1][0];
+        for(int j=1;j<N;j++)
+            maxi= max(maxi, dp[N-1][j]);
         return maxi;
+        
+        //      SPACE OPTIMISATION
     }
 };
 
