ee253868fb1ea198d05712cdbdecc0948f192f71
Devansh-G <75668039+Devansh-G@users.noreply.github.com>
2023-08-10 21:54:10 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,37 +6,71 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(int &node, vector<int> &vis, vector<int> &pathVis, vector<int> adj[])
-    {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        for(auto adjacentNode : adj[node])
-        {
-            if(!vis[adjacentNode])
-            {
-                if(dfs(adjacentNode, vis, pathVis, adj)==true)
-                    return true;
-            }
-            else if(pathVis[adjacentNode])
-                return true;
-        }
-        pathVis[node] = 0;
-        return false;
-    }
+    
+    //DFS
+    // bool dfs(int &node, vector<int> &vis, vector<int> &pathVis, vector<int> adj[])
+    // {
+    //     vis[node] = 1;
+    //     pathVis[node] = 1;
+    //     for(auto adjacentNode : adj[node])
+    //     {
+    //         if(!vis[adjacentNode])
+    //         {
+    //             if(dfs(adjacentNode, vis, pathVis, adj)==true)
+    //                 return true;
+    //         }
+    //         else if(pathVis[adjacentNode])
+    //             return true;
+    //     }
+    //     pathVis[node] = 0;
+    //     return false;
+    // }
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        // for a better space optimised solution see 210. Course Schedule II on leetcode
-        vector<int> vis(V,0);
-        vector<int> pathVis(V,0);
-        for(int i = 0 ;i<V;i++)
+        // DFS
+        // vector<int> vis(V,0);
+        // vector<int> pathVis(V,0);
+        // for(int i = 0 ;i<V;i++)
+        // {
+        //     if(!vis[i])
+        //     {
+        //         if(dfs(i, vis, pathVis, adj)==true)
+        //             return true;
+        //     }
+        // }
+        // return false;
+        
+        //Using BFS or Kahn's Algo
+        vector<int> indegrees(V,0);
+	   for(int i=0 ;i< V; i++)
+	   {
+	       for(auto node : adj[i])
+	           indegrees[node]++;
+	   }
+	   
+	    int count=0;
+	    queue<int> q;
+	    for(int i =0 ; i<V ; i++)
+	    {
+	        if(indegrees[i] == 0)
+	            q.push(i);
+        }
+        while(!q.empty())
         {
-            if(!vis[i])
+            int node = q.front();
+            q.pop();
+            count++;
+            for(auto adjacentNode : adj[node])
             {
-                if(dfs(i, vis, pathVis, adj)==true)
-                    return true;
+                indegrees[adjacentNode]--;
+                if(indegrees[adjacentNode]==0)
+                    q.push(adjacentNode);
             }
         }
-        return false;
+        
+        if(count == V)
+            return false;
+        return true;
     }
 };
 
