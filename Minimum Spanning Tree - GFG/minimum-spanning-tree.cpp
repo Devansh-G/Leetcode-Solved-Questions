b51e8f8cdb5b335df03775fca03f2e944c15429b
Devansh-G <75668039+Devansh-G@users.noreply.github.com>
2023-08-15 01:52:15 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -7,36 +7,86 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int findPar(int u, vector<int> &par)
+    {
+        if(u==par[u])
+            return u;
+        return par[u] = findPar(par[u], par);
+	}
+	void Union(int u, int v, vector<int> &size, vector<int> &par)
+	{
+        int ulp_u = findPar(u, par);
+        int ulp_v = findPar(v, par);
+        if(ulp_u == ulp_v)
+            return;
+        if(size[ulp_u] < size[ulp_v])
+        {
+            par[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            par[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+	}
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        // vector<vector<pair<int, int>>> edges(V);
-        // for(auto edge : adj)
-        // {
-        //     edges[edge[0]].push_back({edge[1], edge[2]});
-        //     edges[edge[1]].push_back({edge[0], edge[2]});
-        // }
-        priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int , int>>> pq;
-        // we will need parent if we want to store the MST, for length we just need node and weight
-        pq.push({0,0});
-        vector<int> vis(V,0);
-        int sum = 0;
+        //Prims ALGO
+        // priority_queue<pair<int,int>, vector<pair<int, int>>, greater<pair<int , int>>> pq;
+        // // we will need parent if we want to store the MST, for length we just need node and weight
+        // pq.push({0,0});
+        // vector<int> vis(V,0);
+        // int sum = 0;
         
-        while(!pq.empty())
+        // while(!pq.empty())
+        // {
+        //     int wt = pq.top().first;
+        //     int node = pq.top().second;
+        //     pq.pop();
+        //     if(vis[node])
+        //         continue;
+        //     vis[node] = 1;
+        //     sum += wt;
+        //     for(auto it : adj[node])
+        //     {
+        //         pq.push({it[1], it[0]});
+        //     }
+        // }
+        // return sum;
+        
+        //KRUSKALS ALGO
+        vector<pair<int, pair<int, int>>>v;
+        for(int i = 0; i<V ; i++)
         {
-            int wt = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            if(vis[node])
-                continue;
-            vis[node] = 1;
-            sum += wt;
-            for(auto it : adj[node])
+            for(auto it : adj[i])
             {
-                pq.push({it[1], it[0]});
+                v.push_back({it[1], {i, it[0]}});
             }
         }
-        return sum;
+        sort(v.begin(), v.end());
+        
+        vector<int> size(V, 1), par(V);
+        for(int i=0;i<V;i++)
+        {
+            par[i] = i;
+        }
+        int ans = 0;
+        
+        for(auto it : v)
+        {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(findPar(u,par) != findPar(v, par))
+            {
+                Union(u, v, size, par);
+                ans+=wt;
+            }
+        }
+        return ans;
     }
 };
 
